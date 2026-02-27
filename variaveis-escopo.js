44f8c29984925ab8baf4df6f8b27889cab076ec5
Elder Gomes <eldergomesdas@gmail.com>
2026-02-27 10:01:04 -0300

    tipos de variaveis null undefined e number

---
+++ variaveis-escopo.js
@@ -0,0 +1,12 @@
+{ 
+    const idade = 30;
+{
+    console.log(idade);
+    let nome ="Elder";
+    console.log(nome);
+}
+ 
+console.log(idade);
+}
+
+ 
---
+++ variaveis-tipos.js
@@ -0,0 +1,14 @@
+let nome ='Elder'; // string
+let idade = 41; //number
+let altura = 1.79; //number
+
+let nota ;
+
+console.log(nota); // undefined
+
+nota = 10;
+console.log(nota); // 10
+
+nota = null;
+console.log(nota);// null
+ 
